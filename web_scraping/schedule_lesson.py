a72277ccfd8dcea8e12b470934a56f07ae423478
Sergioarg <sergio_ramos53@yahoo.com>
2022-10-23 19:04:20 -0500

    Create test files

--- web_scraping/schedule_lesson.py
+++ web_scraping/schedule_lesson.py
@@ -5,6 +5,7 @@ from datetime import datetime as date
 from time import sleep
 
 from selenium import webdriver
+from send_email import send_emial
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
@@ -172,6 +173,7 @@ def schedule_clases():
         warning_message = '//*[@id="TABLE2"]/tbody/tr[3]/td/div/span/div'
         wait.until(EC.presence_of_element_located((By.XPATH, warning_message)))
         print('Class could not be scheduled.')
+        send_emial(text_file)
     except RuntimeError:
         print('Class successfully scheduled.')
 
--- web_scraping/send_email.py
+++ web_scraping/send_email.py
@@ -1,3 +1,26 @@
 import smtplib
 
-# Todo: create script to send emial
+from email.mime.text import MIMEText
+
+
+
+def send_emial(textfile):
+    """send_emial - this function send an emial
+
+    Args:
+        text_file (str): text to send to email
+    """
+    with open(textfile, 'rb') as fb:
+        # Create text/plain message
+        msg = MIMEText(fb.read)
+
+    me = 'sergio_ramos63@yahoo.com'
+    you = 'est.sramos227@smart.edu.co'
+
+    msg['Subject'] = 'The contents of %s' % textfile
+    msg['From'] = me
+    msg['To'] = you
+
+    s = smtplib.SMTP('localhost')
+    s.sendmail(me, [you], msg.as_string())
+    s.quit()
