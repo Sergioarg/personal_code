4465a9feadf5fe1fe342091c2293fe62e33417a2
Sergioarg <sergio_ramos53@yahoo.com>
2023-01-15 21:38:13 -0500

    Crete script to send emial

--- web_scraping/send_email.py
+++ web_scraping/send_email.py
@@ -1,26 +1,50 @@
+#!/usr/bin/env python3
 import smtplib
+import ssl
 
+# Email libraries import
+from email import encoders
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
+G = "\033[92m"
+Re = "\033[m"
 
 
-def send_emial(textfile):
+def send_emial(email_from, password, email_to, subject):
     """send_emial - this function send an emial
 
     Args:
-        text_file (str): text to send to email
     """
-    with open(textfile, 'rb') as fb:
-        # Create text/plain message
-        msg = MIMEText(fb.read)
 
-    me = 'sergio_ramos63@yahoo.com'
-    you = 'est.sramos227@smart.edu.co'
+    # subject = 'Created succesfully.'
+    message = MIMEMultipart()
+    message['From'] = email_from
+    message['To'] = email_to
+    message['Subject'] = subject
 
-    msg['Subject'] = 'The contents of %s' % textfile
-    msg['From'] = me
-    msg['To'] = you
+    text = "Schedule of class for the day created succesfully."
 
-    s = smtplib.SMTP('localhost')
-    s.sendmail(me, [you], msg.as_string())
-    s.quit()
+    part1 = MIMEText(text, 'plain')
+    message.attach(part1)
+
+    # text = message.as_string()
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(email_from, password)
+        server.sendmail(email_from, email_to, text)
+
+    print(f'{G}Email Reminder Sent.{Re}')
+
+
+if __name__ == '__main__':
+
+    email_from = ''
+    password = ''
+    email_to = ''
+    subject = '.'
+
+    send_emial(email_from, password, email_to, subject)
--- web_scraping/send_emial_test.py
+++
@@ -1,3 +0,0 @@
-email_sender = 'sergioa.ramosg53@gmail.com'
-email_password = ''
-emial_reciver = 'est.sramos227@smart.edu.co'
