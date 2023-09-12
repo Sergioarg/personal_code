95b63ba5396f6840cd28c6d60cb2c4e55fcb86ae
sergioarg <sergio_ramos53@yahoo.com>
2023-09-12 12:09:51 -0500

    Update

--- web_scraping/schedule_clases.py
+++ web_scraping/schedule_clases.py
@@ -94,76 +94,60 @@ def schedule_clases(user: str, password: str, next_reps: int):
     button_siguiente = '//*[@id="Grid1ContainerTbl"]/tfoot/tr/td/div/button[3]'
     wait.until(EC.presence_of_element_located((By.XPATH, button_siguiente)))
 
-    # ? Repetir veces que se repite dar clink en siguiente
-    for repetition in range(next_reps):
-        sleep(5)
-        driver.find_element(By.XPATH, button_siguiente).click()
+    stados_de_las_clases = '//*[@id="vTPEAPROBO"]/option[3]'
+    click_element(stados_de_las_clases)
 
     sleep(5)
+    table_xpath = '//*[@id="Grid1ContainerTbl"]/tbody'
+    table = driver.find_element(By.XPATH, table_xpath)
 
-    # ? Create dict comprenhention to clases
-    # ? Note: Change range number as needed
-
-    clases = create_clases_dictionary(3, 9, 3, 9)
-
-    for clase_name, clase_xpath in clases.items():
-
-        clase = wait.until(EC.presence_of_element_located((By.XPATH, clase_xpath)))
-
-        if clase.text == 'Pendiente':
-            print(f'Clase: {clase_name}')
-            clase_row = driver.find_element(By.XPATH, clase_xpath)
+    sleep(5)
+    rows = table.find_elements(By.TAG_NAME, 'tr')
+    for row in rows:
+        if 'CLASE' in row.text:
+            clase = row
+            print(f"Text: {row.text}")
             break
 
-    clase_row.click()
-    sleep(5)
+    clase.click()
 
     # Click on 'Asignar' button
     asignar_button = '//*[@id="BUTTON1"]'
-    driver.find_element(By.XPATH, asignar_button).click()
+    click_element(asignar_button)
 
-    fail_schedule = None
+    # fail_schedule = None
     # ! Close in case of class may not have been scheduled.
-    try:
-        warning_message = '//*[@id="TABLE2"]/tbody/tr[1]/td/div/span/div'
-        fail_schedule = wait.until(EC.presence_of_element_located((By.XPATH, warning_message)))
-        print(f'{Red}Class could not be scheduled.{Reset}')
-        driver.close()
-        driver.quit()
-    except RuntimeError:
-        print(f'{Green}Class successfully scheduled.{Reset}')
+    # try:
+    #     warning_message = '//*[@id="TABLE2"]/tbody/tr[1]/td/div/span/div'
+    #     fail_schedule = wait.until(EC.presence_of_element_located((By.XPATH, warning_message)))
+    #     print(f'{Red}Class could not be scheduled.{Reset}')
+    #     driver.close()
+    #     driver.quit()
+    # except RuntimeError:
+    #     print(f'{Green}Class successfully scheduled.{Reset}')
 
-    if fail_schedule is not None:
-        return (1)
+    # if fail_schedule is not None:
+    #     return (1)
 
     # Return to main main page
     driver.switch_to.default_content()
-
     # SELECT sede, dia and hora of the class
     second_iframe = '//*[@id="gxp1_ifrm"]'
     wait.until(EC.presence_of_element_located((By.XPATH, second_iframe)))
 
+
     sleep(3)
     selecion_clases_iframe = driver.find_element(By.XPATH, second_iframe)
     driver.switch_to.frame(selecion_clases_iframe)
-    # Display dropdown of sede
-    sede_dropdown = '//*[@id="vREGCONREG"]'
-    driver.find_element(By.XPATH, sede_dropdown).click()
-
-    # Select CALIMA sede in the dropdown, change the number acording the class
-    calima_option = '//*[@id="vREGCONREG"]/option[14]'
-    driver.find_element(By.XPATH, calima_option).click()
 
     # Display dropdown of day
     dia_dropdown = '//*[@id="vDIA"]'
     driver.find_element(By.XPATH, dia_dropdown).click()
+    sleep(3)
 
     dia_selection = '//*[@id="vDIA"]/option[2]'
     driver.find_element(By.XPATH, dia_selection).click()
-    driver.find_element(By.XPATH, dia_selection).click()
 
-    # if current_day == 'Wednesday':
-    #     ultima_clase = '//*[@id="Grid1ContainerRow_0011"]/td[3]'
     ultima_clase = '//*[@id="Grid1ContainerRow_0010"]/td[3]'
     click_element(ultima_clase)
 
